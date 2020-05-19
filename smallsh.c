0b268f18d4a6f986427e178aedf052658cda9c3c
Isaac Beale <bealei@oregonstate.edu>
2020-05-19 15:29:03 -0700

    file redirects and bg processes

--- smallsh.c
+++ smallsh.c
@@ -3,6 +3,7 @@
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 
 #define STR_BUF 2048
 
@@ -12,10 +13,18 @@ void catchSIGINT(int signo){
     exit(0);
 }
 
+struct backgroundProcessPIDs{
+    int num_processes;
+    int PIDs[100];
+};
+
 
 int main(){
     int i;
     int j;
+    int backgroundFlag = 0;
+    struct backgroundProcessPIDs BGPids;
+    BGPids.num_processes = 0;
     struct sigaction SIGINT_action = {0};
     SIGINT_action.sa_handler = catchSIGINT;
     sigfillset(&SIGINT_action.sa_mask);
@@ -24,11 +33,26 @@ int main(){
     char* buffer;
     buffer = (char*) malloc(STR_BUF);
     int num_args;
+    int outputfileloc;
+    int inputfileloc;
+    int outputflag = 0;
+    int inputflag = 0;
     ssize_t buffer_size = STR_BUF;
     ssize_t inputsize = 0;
+    
     int exitflag = 0;
     do{
+        backgroundFlag = 0;
+        int childExitStatus = -5;
+        for(i=0; i < BGPids.num_processes; i++){
+            int finished = waitpid(BGPids.PIDs[i], &childExitStatus, WNOHANG);
+            if(finished){
+                BGPids.num_processes--;
+            }
+        }
         while(1){
+            outputflag = 0;
+            inputflag = 0;
             for(i = 0; i < 513; i++){
                 args[i] = NULL;
             }
@@ -64,15 +88,27 @@ int main(){
                 arg = strdup(newarg);
                 free(newarg);
             }
+            else if(arg[0] == '#'){
+                break;
+            }
+            else if(arg[0] == '>'){
+                outputfileloc = num_args + 1;
+                outputflag = 1;
+            }
+            else if(arg[0] == '<'){
+                inputfileloc = num_args + 1;
+                inputflag = 1;
+            }
             args[num_args] = arg;
             arg = strtok(NULL, delim);
             num_args++;
         }
-        
-        for(i = 0;i<num_args;i++){
-            printf("%s\n", args[i]);
-            fflush(stdin);
+
+        if(!strcmp(args[num_args-1], "&")){ // check if last argument is &, and set background flag.
+            backgroundFlag = 1;
+            args[num_args-1] = NULL;
         }
+        
         if(strcmp("cd", args[0])==0){
             if(args[1])
                 chdir(args[1]);
@@ -82,7 +118,6 @@ int main(){
         // Run all other commands with fork() and execvp()
         else{
             pid_t spawnpid = -5;
-            int childExitStatus = -5;
             spawnpid = fork();
             switch(spawnpid){
                 case -1: // If spawnpid is -1 there was some error forking.
@@ -90,13 +125,44 @@ int main(){
                     fflush(stdin);
                     exit(1);break;
                 case 0: //If its a child, then we turn it into an execvp() process and execute whatever is in the arguments
+                    if(inputflag){
+                        int inputTarget = open(args[inputfileloc], O_RDONLY);
+                        dup2(inputTarget, 0);
+                        args[inputfileloc - 1] = NULL;
+                        args[inputfileloc] = NULL;
+                        close(inputTarget);
+                    }
+                    if(outputflag){
+                        int outputTarget = open(args[outputfileloc], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                        dup2(outputTarget, 1);
+                        args[outputfileloc - 1] = NULL;
+                        args[outputfileloc] = NULL;
+                        close(outputTarget);
+                    }
+                    if(outputflag == 0 && backgroundFlag == 1){
+                        int devnull = open("/dev/null", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                        dup2(devnull, 1);
+                        close(devnull);
+                    }
+                    if(inputflag == 0 && backgroundFlag == 1){
+                        int devnull = open("/dev/null", O_RDONLY);
+                        dup2(devnull, 0);
+                        close(devnull);
+                    }
                     execvp(args[0], args);
+                    
                     fflush(stdin);
                     exit(0);
                     break;
                 default: // The parent will wait for the child process to exit.
-                    waitpid(spawnpid, &childExitStatus, 0);
+                    if(backgroundFlag == 0)
+                        waitpid(spawnpid, &childExitStatus, 0);
+                    else{
+                        BGPids.PIDs[BGPids.num_processes] = spawnpid;
+                        BGPids.num_processes++;
+                    }
             }
+            
         }
     }while(exitflag == 0);
     return 0;
